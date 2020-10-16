182dd1d491e7f126cb1302354a47a2be152a6b82
Rashel <rashel.gandhi@gmail.com>
2020-10-16 14:58:54 -0600

    Updates

---
+++ precache-manifest.f3cde43a685841ce68117b82de4ea08c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a81ec0f4ddba6b90f3b8cc65fe6b81",
+    "url": "/raga4905/project-4-assessment.git/index.html"
+  },
+  {
+    "revision": "b9f572473090ad573a0a",
+    "url": "/raga4905/project-4-assessment.git/static/css/main.c0835aaf.chunk.css"
+  },
+  {
+    "revision": "608118624ae7ee197d4b",
+    "url": "/raga4905/project-4-assessment.git/static/js/2.e208ecfd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/raga4905/project-4-assessment.git/static/js/2.e208ecfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f572473090ad573a0a",
+    "url": "/raga4905/project-4-assessment.git/static/js/main.49d8555b.chunk.js"
+  },
+  {
+    "revision": "976deaf7d4dcbf043fe9",
+    "url": "/raga4905/project-4-assessment.git/static/js/runtime-main.449c6730.js"
+  }
+]);
